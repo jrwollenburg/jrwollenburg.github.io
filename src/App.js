6165a73e68d82bea1ac80b4c1282967173789288
James Wollenburg <wollenbj@oregonstate.edu>
2023-07-11 21:11:53 -0500

    fix6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Link to="/about">About</Link>
+      <Link to="/About">About</Link>
      <h2>Github Pages</h2>
      <h3>Deploying React to Github</h3>
     </div>
